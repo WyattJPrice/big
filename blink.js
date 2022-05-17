e5661271a2525d5ff2f93c0007b3b4614a71e14a
Aerolix_school <79605302+Aerolixdev@users.noreply.github.com>
2022-05-17 09:29:31 -0500

    s

--- blink.js
+++ blink.js
@@ -1,6 +1,13 @@
 function start() {
     var thetext = prompt("Text");
     document.getElementById("output").innerHTML = thetext; 
+    setInterval(blink(), 500);
 }
 
+function blink() {
+    var thebody = document.getElementById("thebody");
+    var theoutput = document.getElementById("output");
+    thebody.style.backgroundColor = 'black';
+    theoutput.style.backgroundColor = 'white'; 
+}
 
