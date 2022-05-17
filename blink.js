25c07ed719ec3960afe90c54ada2ed72f5f64124
Aerolix_school <79605302+Aerolixdev@users.noreply.github.com>
2022-05-17 10:06:08 -0500

    stoop

--- blink.js
+++ blink.js
@@ -4,14 +4,7 @@ var theoutput = document.getElementById("output");
 function start() {
     var thetext = prompt("Text");
     document.getElementById("output").innerHTML = thetext; 
-    var list = [1,2,3,4];
-
-    for (var x = 0, ln = list.length; x < ln; x++) {
-    setTimeout(function(y) {    
-    thebody.style.backgroundColor = 'black';
-    theoutput.style.color = 'white'; 
-    }, x * 500, x); // we're passing x
-    }
+    theoutput.classList.add("addanimation");
 }
 
 
