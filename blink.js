fa074267e81d61336405f119f5b93719d44916ac
Aerolix_school <79605302+Aerolixdev@users.noreply.github.com>
2022-05-17 09:47:37 -0500

    s

--- blink.js
+++ blink.js
@@ -1,10 +1,17 @@
+var thebody = document.getElementById("thebody");
+var theoutput = document.getElementById("output");
+
 function start() {
-    var thebody = document.getElementById("thebody");
-    var theoutput = document.getElementById("output");
     var thetext = prompt("Text");
     document.getElementById("output").innerHTML = thetext; 
-    setInterval(thebody.style.backgroundColor = 'black';
-    theoutput.style.color = 'white'; , 500);
+    var list = [1,2,3,4];
+
+    for (var x = 0, ln = list.length; x < ln; x++) {
+    setTimeout(function(y) {    
+    thebody.style.backgroundColor = 'black';
+    theoutput.style.color = 'white'; 
+    }, x * 500, x); // we're passing x
+    }
 }
 
 
